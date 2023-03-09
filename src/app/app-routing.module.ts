9f9d2f4fd51f281592ed255453acb8a6a27b9f84
Jessroa3 <jessicaroa3@gmail.com>
2023-03-08 21:16:04 -0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './layouts/landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'full'},
+  { path: 'index', component: LandingComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
